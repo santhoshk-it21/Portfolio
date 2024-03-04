f3832397a234f9dc44b09ea2d413c58535fa5de4
santhoshk-it21 <95066902+santhoshk-it21@users.noreply.github.com>
2024-03-04 23:08:52 +0530

    Update Second.js

--- src/components/Second.js
+++ src/components/Second.js
@@ -2,7 +2,7 @@ import backpic from "../assets/backpic.jpg";
 const Second = () => {
   return (
     <div className="" id="second">
-      <img src={backpic} />
+      <img src={backpic} alt="Manikandan" />
       <div className="hidden md:block">
         <h1 className="backtext font-hero text-8xl ">Mani Kandan</h1>
         <p className="backpra font-hero hover:text-white"> scroll down</p>
