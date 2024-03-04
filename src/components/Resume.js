2f2ee5aa6252fa6a55de6b309c3cd017c8ec9611
santhoshk-it21 <95066902+santhoshk-it21@users.noreply.github.com>
2024-03-04 23:27:15 +0530

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,31 +1,44 @@
-import resume from "../assets/resume.png";
+import React from 'react';
+import resume from '../assets/resume.png';
+import pdfFile from '../assets/Resume.pdf'; // Assuming you have the correct path to your PDF file
 
 const Resume = () => {
+  const handleDownload = () => {
+    // Create a link element
+    const link = document.createElement('a');
+    link.href = pdfFile;
+    link.target = '_blank'; // Open the link in a new tab/window
+    link.download = 'Resume.pdf'; // Set the default download filename
+
+    // Append the link to the document and trigger a click
+    document.body.appendChild(link);
+    link.click();
+
+    // Remove the link from the document
+    document.body.removeChild(link);
+  };
+
   return (
-    <thrid className="flex flex-col md:flex-row backfirst px-5" id="resume">
+    <div className="flex flex-col md:flex-row backfirst px-5" id="resume">
       <div className="md:w-1/2 flex justify-center py-20">
-        <img className="h-[500px] py-3" src={resume}  alt="Manikandan"/>
+        <img className="h-[500px] py-3" src={resume} alt="Resume" />
       </div>
-      <div className=" md:w-1/2 flex justify-center font-hero ">
-        <div className=" w-1/2 flex flex-col justify-center text">
-          <h1 className="text-5xl  py-5">Resume</h1>
+      <div className="md:w-1/2 flex justify-center font-hero">
+        <div className="w-1/2 flex flex-col justify-center text">
+          <h1 className="text-5xl py-5">Resume</h1>
           <p className="text-white">
             Experienced Full Stack Developer with proficiency in front-end
             technologies like React, PWA and also back-end technologies. Basic in
-            database management with MongoDB. .
+            database management with MongoDB.
             <br />
           </p>
-          <button className="btn">
-            <a
-              className=" flex py-6"
-              href="https://drive.google.com/file/d/1DcykCvh56j7VH7ZK3o34xmcOheRdEynr/view?usp=drive_link"
-            >
-              Download
-            </a>
-          </button>{" "}
+          <button className="btn flex py-6" onClick={handleDownload}>
+            Download
+          </button>
         </div>
       </div>
-    </thrid>
+    </div>
   );
 };
+
 export default Resume;
